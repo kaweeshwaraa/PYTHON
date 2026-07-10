4a30fbcd41599f8b4080730f68f940a2f30d7c28
kaweeshwaraa <kaweeshwara0@gmail.com>
2026-07-10 12:03:04 +0530

    day01 commit

---
+++ day01/day01.py
@@ -0,0 +1,12 @@
+# print("Hello World \nKing Kong")
+# print("Hello" + "4")
+# name = input("Hey What's your name? ")
+# print("Hello " + name)
+# print(len(name))
+
+#Exercise 01 of day 01
+
+print("Welcome to the Band Name Generator")
+cityName = input("What's the name of the city you grew up in? ")
+petName = input("What's the pet name? ")
+print("Your band name could be " + cityName + " " + petName)
