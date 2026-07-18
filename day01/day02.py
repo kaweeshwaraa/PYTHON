00473614c09284bfe2bb9595354168f7e8ea7297
kaweeshwaraa <kaweeshwara0@gmail.com>
2026-07-18 19:28:39 +0530

    Update day 02

--- day01/day02.py
+++ day01/day02.py
@@ -2,10 +2,31 @@
 # print("Hello " + name + "\n" + "The number of character in your name is " + str(len(name)))
 
 #day 02 Exercise
-print("Welcome to the tip Calculator!")
-totalBill = float(input("What was the total bill? $"))
-tipAmount = int(input("How much tip would you like to give? 10, 12 , or 15 ?"))
-noOfPeople = int(input("How many people to split the bill ?"))
-print("Each person should pay " + str(round((totalBill + ((totalBill * tipAmount) / 100)) / noOfPeople, 2)))
+# print("Welcome to the tip Calculator!")
+# totalBill = float(input("What was the total bill? $"))
+# tipAmount = int(input("How much tip would you like to give? 10, 12 , or 15 ?"))
+# noOfPeople = int(input("How many people to split the bill ?"))
+# print("Each person should pay " + str(round((totalBill + ((totalBill * tipAmount) / 100)) / noOfPeople, 3)))
 
 
+# # print("Hello"[])
+# name = "Kawe"
+#
+# print(type("Kawe"))
+# print(type(123))
+# print(type(1.23))
+# print(type(True))
+
+
+# amount= "1234"
+# print((int(amount) + 1))
+
+# print("Number of letters in your name: " + str(len(input("What 's your name? "))))
+
+# print(2 ** 5)
+
+is_winning = True
+name = "Kawe"
+age = 36
+
+print(f"Your name is {name} and you age is {age} and you are winning {is_winning} ")
