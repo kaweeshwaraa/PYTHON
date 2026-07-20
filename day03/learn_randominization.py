e0783e221143a9ff3512035e950d0955c032e1fd
kaweeshwaraa <kaweeshwara0@gmail.com>
2026-07-20 14:52:51 +0530

    day04

---
+++ day03/learn_randominization.py
@@ -0,0 +1,31 @@
+import random
+from math import floor
+
+import my_module
+
+# print (random.randint(1,3))
+# print(my_module.my_fav_no)
+
+
+# print(floor(random.random()*10))
+
+# print("Welcome to head and tails Game Nigger")
+# random_value = random.randint(1, 2)
+# if random_value == 1:
+#     print("You got  head")
+# elif random_value == 2:
+#     print("You got  tail")
+
+# It just clicks now i don't even know tbh
+
+names = ["kawe", "ilikekawe","jayJ"]
+names.append("KaweNito")
+
+print(names)
+
+# random_value = random.randint(0,len(names)-1)
+#
+# print(f"Today you will be {names[random_value]}")
+
+
+t
