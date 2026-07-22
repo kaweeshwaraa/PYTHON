348f6257932ad699afb901b63845d99d4234aeb8
kaweeshwaraa <kaweeshwara0@gmail.com>
2026-07-23 04:40:21 +0530

    day05

---
+++ day05/learning_function_for_fun.py
@@ -0,0 +1,9 @@
+# def my_first_amazing_super_cool_function(name):
+#     print("Hi Cool Guy "+ name)
+#
+# my_first_amazing_super_cool_function( "Da  ve")
+
+# age = 1
+# while age <= 10:
+#     print(age)
+#     age+=1
